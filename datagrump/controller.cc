c216c770a3554fba5983eb07afe7d5ad2d3f086c
Mitchell <mdumovic@gmail.com>
2017-04-25 16:01:41 -0700

    tcp cubic results promising but needs work, power around 25

--- datagrump/controller.cc
+++ datagrump/controller.cc
@@ -1,72 +1,92 @@
 #include <iostream>
+#include <math.h>
 
 #include "controller.hh"
 #include "timestamp.hh"
 
 using namespace std;
 
+#define DELAY_TRIGGER_THRESH 100
+
 /* Default constructor */
 Controller::Controller( const bool debug )
-  : debug_( debug ), cwnd(10), counter(0),
-  prev_rtt(0), min_rtt(0), rtt_diff(0),
-  updateParameter(1)
+  : debug_( debug ), last_drop_time(0), wmax(20),
+  beta(0.5), C(3.5), last_window(0)
+
 {
+  last_window = (1 - beta) * wmax;
   debug_ = true;
 }
 
 /* Get current window size, in datagrams */
 unsigned int Controller::window_size( void )
 {
-  if ( debug_ ) {
-    cerr << "At time " << timestamp_ms()
-	 << " window size is " << cwnd << endl;
+  double K = beta * wmax / C;
+  K = cbrt(K);
+  uint64_t t = timestamp_ms() - last_drop_time;
+  double t_d = (t / 1000.0);
+  double wcubic = (C * pow(t_d - K, 3.0)) + (double)wmax;
+  cerr << "K = " << K << ", t = " << t << ", t_d = " << t_d << ", last_window = " << last_window << ", wcubic = " << wcubic << endl;
+  if (wcubic < 1.0) {
+    wcubic = 1.0;
   }
 
-  return cwnd;
+  double smin = 1 / last_window;
+  double window_diff = wcubic - last_window;
+  if (window_diff < 0) {
+    last_window = wcubic;
+    window_diff = 0.0;
+  }
+
+  if (window_diff < smin) {
+    cerr << "smaller than smin" << endl;
+    wcubic = last_window + smin;
+  }
+
+  if ( debug_ ) {
+    cerr << "At time " << timestamp_ms()
+   << " window size is " << (unsigned int) wcubic << endl;
+  }
+
+  return (unsigned int)wcubic;
 }
 
 /* A datagram was sent */
 void Controller::datagram_was_sent( const uint64_t sequence_number,
-				    /* of the sent datagram */
-				    const uint64_t send_timestamp )
+            /* of the sent datagram */
+            const uint64_t send_timestamp )
                                     /* in milliseconds */
 {
   /* Default: take no action */
 
   if ( debug_ ) {
     cerr << "At time " << send_timestamp
-	 << " sent datagram " << sequence_number << endl;
-  }
-}
-
-void Controller::updateAcks(int n) {
-  counter+=n;
-  while (counter >= ((int)cwnd)) {
-    cwnd++;
-    counter-=cwnd;
-  }
-
-  while (cwnd > 0 && counter <= (-1 * ((int)cwnd))) {
-    cwnd--;
-    counter+= cwnd;
-  }
-
-  if (cwnd <= 0) {
-    cwnd = 1;
-    counter = 0;
+   << " sent datagram " << sequence_number << endl;
   }
 }
 
 /* An ack was received */
 void Controller::ack_received( const uint64_t sequence_number_acked,
-			       /* what sequence number was acknowledged */
-			       const uint64_t send_timestamp_acked,
-			       /* when the acknowledged datagram was sent (sender's clock) */
-			       const uint64_t recv_timestamp_acked,
-			       /* when the acknowledged datagram was received (receiver's clock)*/
-			       const uint64_t timestamp_ack_received )
+             /* what sequence number was acknowledged */
+             const uint64_t send_timestamp_acked,
+             /* when the acknowledged datagram was sent (sender's clock) */
+             const uint64_t recv_timestamp_acked,
+             /* when the acknowledged datagram was received (receiver's clock)*/
+             const uint64_t timestamp_ack_received )
                                /* when the ack was received (by sender) */
 {
+  uint64_t rtt = timestamp_ack_received - send_timestamp_acked;
+  uint64_t t = timestamp_ack_received - last_drop_time;
+  if (t > 2 * DELAY_TRIGGER_THRESH
+    && rtt > DELAY_TRIGGER_THRESH) {
+    cerr << "cut" << endl;
+    wmax = window_size();
+    last_drop_time = timestamp_ack_received;
+    last_window = (1 - beta) * wmax;
+  } else {
+    last_window = window_size();
+  }
+
   if ( debug_ ) {
     cerr << "At time " << timestamp_ack_received
    << " received ack for datagram " << sequence_number_acked
@@ -74,51 +94,15 @@ void Controller::ack_received( const uint64_t sequence_number_acked,
    << ", received @ time " << recv_timestamp_acked << " by receiver's clock)"
    << endl;
   }
-  long long new_rtt = timestamp_ack_received - send_timestamp_acked;
-  if (min_rtt == 0) {
-    min_rtt = new_rtt;
-    prev_rtt = new_rtt;
-    return;
-  }
+}
 
-  if (new_rtt < min_rtt) {
-    min_rtt = new_rtt;
-  }
-
-  double new_rtt_diff = new_rtt - prev_rtt;
-  prev_rtt = new_rtt;
-
-  if (new_rtt > rtt_high) {
-    updateParameter = 1;
-    cerr << "outlier occurs" << endl;
-    // int counterDecrease = cwnd * (1 - (beta * (1 - (rtt_high / ((double)(new_rtt))))));
-    // updateAcks(-4 * counterDecrease);
-    cwnd /= 2;
-    if (cwnd == 0) cwnd = 1;
-    counter = 0;
-    return;
-  }
-
-  rtt_diff = ((1.0 - alpha) * rtt_diff) + (alpha * new_rtt_diff);
-  double normalized_gradient = rtt_diff / min_rtt;
-  cerr << "new rtt: " << new_rtt << "new_rtt_diff = " << new_rtt_diff << ", min_rtt = " << min_rtt << ", rtt_diff = " << rtt_diff << ", normalized_gradient = " << normalized_gradient << endl;
-  if (normalized_gradient <= 0) {
-    cerr << "Adding one to window" << endl;
-    updateAcks(4 * updateParameter);
-    updateParameter++;
-  } else {
-    updateParameter = 1;
-    cerr << "Decreasing from gradient by factor of " << (1 - (beta * normalized_gradient)) <<  endl;
-    int counterDecrease = cwnd * (1 - (beta * normalized_gradient));
-    updateAcks(-1 * counterDecrease);
-  }
-
-  
+void Controller::timeout_occured( void ) {
+  // cwnd = 1;
 }
 
 /* How long to wait (in milliseconds) if there are no acks
    before sending one more datagram */
 unsigned int Controller::timeout_ms( void )
 {
-  return 1000; /* timeout of one second */
+  return 50; /* timeout of one second */
 }
--- datagrump/controller.hh
+++ datagrump/controller.hh
@@ -11,19 +11,11 @@ private:
   bool debug_; /* Enables debugging output */
 
   /* Add member variables here */
-  unsigned int cwnd;
-  int counter;
-  long long prev_rtt;
-  long long min_rtt;
-  double rtt_diff;
-  int updateParameter;
-
-  const long long rtt_high = 150;
-  const long long rtt_low = 50;
-  const double alpha = 0.2;
-  const double beta = 0.8;
-
-  void updateAcks(int n);
+  uint64_t last_drop_time;
+  unsigned int wmax;
+  double beta;
+  double C;
+  double last_window;
   
 public:
   /* Public interface for the congestion controller */
@@ -38,17 +30,19 @@ public:
 
   /* A datagram was sent */
   void datagram_was_sent( const uint64_t sequence_number,
-			  const uint64_t send_timestamp );
+        const uint64_t send_timestamp );
 
   /* An ack was received */
   void ack_received( const uint64_t sequence_number_acked,
-		     const uint64_t send_timestamp_acked,
-		     const uint64_t recv_timestamp_acked,
-		     const uint64_t timestamp_ack_received );
+         const uint64_t send_timestamp_acked,
+         const uint64_t recv_timestamp_acked,
+         const uint64_t timestamp_ack_received );
 
   /* How long to wait (in milliseconds) if there are no acks
      before sending one more datagram */
   unsigned int timeout_ms( void );
+
+  void timeout_occured ( void );
 };
 
 #endif
